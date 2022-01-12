db9b75d2d6c4d455de9f731427605634c767a4d0
Maria Florencia Gonzalez <rightonhana@gmail.com>
2022-01-11 23:23:27 -0800

    :sparkles: add script

---
+++ updateReadme.mjs
@@ -0,0 +1,33 @@
+import { readFile, writeFile } from "node:fs/promises";
+
+const filename = "README.md";
+
+const holydays = {
+	"01/01": "newYear",
+	"09/30": "birthday",
+	"10/31": "halloween",
+	"12/24": "christmas",
+	"12/25": "christmas",
+	"12/31": "newYear"
+};
+
+const date = new Date()
+	.toISOString()
+	.split("T")[0]
+	.split("-")
+	.slice(1)
+	.join("/");
+
+readFile(filename, "utf-8")
+	.then(data =>
+		writeFile(
+			filename,
+			data.replace(
+				/(?<src>src=")(?<path>[^"]*)(?<close>")/u,
+				`$1./variants/${holydays[date] ?? "base"}.svg$3`
+			),
+			"utf-8"
+		)
+	)
+	.then(() => console.log("Update complete."))
+	.catch(console.error);
